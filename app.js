b1d2e32fb6dbe3e841ca87fe61758bed793af5b9
Javier Caicedo <jicaicedocpe@gmail.com>
2018-02-13 22:36:51 -0500

    Paso 7 - leer parametros enviados por formulario con metodo POST

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+/*
+	Paso 7 - Instalar "body-parser"
+
+	7.1 npm install body-parser --save
+	Instalar este paquete para poder leer los parámetros enviados por el formulario
+	usando el método POST
+
+	7.2 require("body-parser")
+
+	7.3 Montar el middleware  app.use()
+	se necesita para peticiones json => app.use(bodyParser.json())
+	y para decodificar las url => app.use(bodyParser.urlencoded({extended: true}))
+		- extended: true = se puede parsear arreglos y muchos más
+		- extended: false = es limitado la opción de parseo
+
+	Para poder leer los parametros se va a tener en cuenta el atributo "name" de las etiquetas
+
+*/
 const path = require('path')
 var express = require('express')
 var cons = require('consolidate')
+var bodyParser = require('body-parser')
 
 var app = express()
 
@@ -10,7 +29,14 @@ app.set('view engine', 'pug')
 
 // Se establece el directorio "static" para guardar todos aquellos archivos que van a ser
 // embebidos por los archivos ".pug", como por ejemplo imagenes, .css, .js, etc.
-app.use( express.static( "static" ) );
+app.use("/static", express.static("static"));
+
+// 
+app.use(bodyParser.json())
+
+// 
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 // // Parametro 
 // // "/" va al raiz
@@ -26,4 +52,14 @@ app.get('/adicionarEstudiante', (req, res) => {
 	res.render('adicionarEstudiante')
 })
 
+// Ruta para recibir los datos enviados por el formulario
+app.post('/resultado', (req, res) => {
+	console.log('Nombre:' + req.body.nombreEstudiante)
+	res.send('Recibidos datos de estudiantes...')
+})
+
 app.listen(8080)
+
+console.log("\n\n\nBienvenidos al sistema de votaciones")
+console.log("\n\nInstitución Educativa Cascajal")
+console.log("\n\n¡======== Iniciado servidor ========!")
