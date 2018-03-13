2bed2c6aa4b35b0bb879931c3b4c8fc7780a6582
Javier Caicedo <jicaicedocpe@gmail.com>
2018-03-13 18:22:10 -0500

    TAG_07 - permite conocer por quien vota cada estudiante NO GUARDA en BD

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.get("/administrador", (req, res) => {
 	res.render("administrador")
 })
 
+// ===========================================================================
+// Pagina inicial: index
+// 
 app.post("/", (req, res) => {
 	console.log("POST /")
 	if( req.body.idUsuario == "Administrador" ) {
@@ -65,6 +68,7 @@ app.get("/", (req, res) => {
 	res.render("index")
 })
 
+// ===========================================================================
 // Consultar estudiante para votar
 app.post('/consultarEstudiantes', (req, res) => {
 	console.log("POST -> consultarEstudiantes: Listado de estudiantes")
@@ -115,21 +119,20 @@ app.get('/estudiantesIECascajal', (req, res) => {
 	})
 })
 
-app.get("/gradosIECascajal", (req, res) => {
-	console.log("GET -> gradosIECascajal")
-	res.render("gradosIECascajal")
+// ============================================================================
+// Votar en la I.E. Cascajal
+app.get('/votarIECascajal', (req, res) => {
+	console.log("GET -> votar IECascajal" + req.body.gradosIECascajal)
+	res.render('votarIECascajal')
 })
 
-app.get('/votarIECascajal', (req, res) => {
-	console.log("GET -> votar IECascajal" + req.body.name)
+app.post('/votarIECascajal', (req, res) => {
 
+	console.log("REQ->NAME-BUTON: " + req.body.votar)
 
-	// Parametros:
-	// @ find( {<condición>} ) = no hay ninguna condición por eso trae todos los registros
-	// @ select( campos o atributos ) = elementos a traer de la base de datos
-	// @callback = (error, docs) = función para capturar errores o registros
+	console.log("POST -> votar IECascajal" + req.body.gradosIECascajal)
 	Estudiante.
-	find({"est_grado": req.body.name}).
+	find({"est_grado": req.body.gradosIECascajal}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		// 2 - Obtener un estudiante mediante el código de estudiante: 'est_ID'		
@@ -138,33 +141,106 @@ app.get('/votarIECascajal', (req, res) => {
 	})
 })
 
-app.get("/personero", (req, res) => {
-	console.log("GET -> personero")
-	res.render("personero")
+// ============================================================================
+// Votar en la Sede El Tobo
+app.get('/votarSedeElTobo', (req, res) => {
+	console.log("GET -> votar votarSedeElTobo" + req.body.gradosSedeElTobo)
+	res.render('votarSedeElTobo')
 })
 
-app.get("/representanteGrado11", (req, res) => {
-	console.log("GET -> representanteGrado11")
-	res.render("representanteGrado11")
+app.post('/votarSedeElTobo', (req, res) => {
+	console.log("POST -> votar votarSedeElTobo" + req.body.gradosSedeElTobo)
+	Estudiante.
+	find({"est_grado": req.body.gradosSedeElTobo, "est_nombre_sede": "EL TOBO"}).
+	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
+	exec( (error, docs) => {
+		// 2 - Obtener un estudiante mediante el código de estudiante: 'est_ID'		
+		let estudiantes = docs
+		res.render('votarSedeElTobo', {estudiantes} )
+	})
 })
+
+// ============================================================================
+// Votar en la Sede La Piragua
+app.get('/votarSedeLaPiragua', (req, res) => {
+	console.log("GET -> votar votarSedeLaPiragua" + req.body.gradosSedeElTobo)
+	res.render('votarSedeLaPiragua')
+})
+
+app.post('/votarSedeLaPiragua', (req, res) => {
+	console.log("POST -> votar votarSedeLaPiragua" + req.body.gradosSedeElTobo)
+	Estudiante.
+	find({"est_grado": req.body.gradosSedeElTobo, "est_nombre_sede": "LA PIRAGUA"}).
+	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
+	exec( (error, docs) => {
+		// 2 - Obtener un estudiante mediante el código de estudiante: 'est_ID'		
+		let estudiantes = docs
+		res.render('votarSedeLaPiragua', {estudiantes} )
+	})
+})
+
+// ============================================================================
+// Votar en la Sede Paquies
+app.get('/votarSedePaquies', (req, res) => {
+	console.log("GET -> votar votarSedePaquies" + req.body.gradosSedeElTobo)
+	res.render('votarSedePaquies')
+})
+
+app.post('/votarSedePaquies', (req, res) => {
+	console.log("POST -> votar votarSedePaquies" + req.body.gradosSedeElTobo)
+	Estudiante.
+	find({"est_grado": req.body.gradosSedeElTobo, "est_nombre_sede": "PAQUIES"}).
+	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
+	exec( (error, docs) => {
+		// 2 - Obtener un estudiante mediante el código de estudiante: 'est_ID'		
+		let estudiantes = docs
+		res.render('votarSedePaquies', {estudiantes} )
+	})
+})
+
+// ============================================================================
+// Votar por Personero
+// 
+app.post("/personero", (req, res) => {
+	console.log("POST -> personero")
+	console.log("No. Identificación estudiante: " + req.body.votar)
+	let est_ID = req.body.votar
+	res.render("personero", {est_ID})
+})
+
+
+// ============================================================================
+// Votar por representante de grado 11
+// 
+// app.get("/representanteGrado11", (req, res) => {
+// 	console.log("GET -> representanteGrado11")
+// 	console.log("personero: " + req.body.personero)
+// 	res.render("representanteGrado11")
+// })
+
+app.post("/representanteGrado11", (req, res) => {
+	console.log("GET -> representanteGrado11")
+	console.log("OK personero: " + req.body.personero)
+	let personero = req.body.personero
+	// console.log("ID-EST: "+est_ID)
+	// res.render("representanteGrado11", {est_ID, personero})
+	res.render("representanteGrado11", {personero})
+})
+
+// ============================================================================
+// Final de votación
+// 
+app.post("/finalProcesoVotacion", (req, res) => {
+	console.log("POST -> finalProcesoVotacion")
+	console.log("OK representante11: " + req.body.representante11)
+	res.render("finalProcesoVotacion")	
+})
+
+// app.get("/finalProcesoVotacion", (req, res) => {
+// 	console.log("GET -> finalProcesoVotacion")
+// 	console.log("representante11: " + req.body.representante11)
+// 	res.render("finalProcesoVotacion")	
+// })
 
 app.listen(8080)
-
-// Ruta para recibir los datos enviados por el formulario
-// ADICIONAR....
-// app.post('/resultado', (req, res) => {
-// 	var estudiante = new Estudiante({
-// 		est_ID: req.body.codigoEstudiante,
-// 		est_nombre: req.body.nombreEstudiante,
-// 		est_apellidos: req.body.apellidoEstudiante,
-// 		est_fecha_nacimiento: req.body.fechaNacimientoEstudiante,
-// 		est_foto: '/static/imagenes/fotos_alumnos/'+req.body.fotoEstudiante
-// 	})
-
-// 	estudiante.save().then( (est) => {
-// 		res.send("Guadado exitosamente")
-// 	}, (error) => {
-// 		res.send("Fallo al guardar en la base de datos")
-// 	})
-// })
 
