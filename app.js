8d6a817caa65cb57bd3dd05fe99b4c46449fc1b4
Javier Caicedo <jicaicedocpe@gmail.com>
2018-03-17 01:11:21 -0500

    TAG_14 - Funciona los usuarios -> Guarda bien votaciones en BD - Resalta y bloquea ya votantes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express')
-const path = require('path')
-var cons = require('consolidate')
-var bodyParser = require('body-parser')
-var Estudiante = require('./models/estudiante').Estudiante
+var express = require("express")
+const path = require("path")
+var cons = require("consolidate")
+var bodyParser = require("body-parser")
+var Estudiante = require("./models/estudiante").Estudiante
 var Votaciones = require("./models/votaciones").Votaciones
 var Votante = require("./models/votante").Votante
 var Usuario = require("./models/usuario").Usuario
@@ -19,8 +19,6 @@ var num_grado_estudiante	// Guarda el grado del estudiante
 var num_id_estudiante		// Guarda el número de identificación del estudiante
 var num_personero			// Guarda el número del personero votado
 var num_representante		// Guarda el número del representante votado
-var ids_estudiantes_ya_votaron = []
-var registros_a_bloquear = []
 
 
 var ids_estudiantes_ya_votaron = []
@@ -31,7 +29,7 @@ var registros_a_bloquear = []
 
 // Establecemos el motor de vistas, es decir tomamos los archivos ".pug" para que express
 // con Node.js los conviertan a archivos ".html" 
-app.set('view engine', 'pug')
+app.set("view engine", "pug")
 
 // Se establece el directorio "static" para guardar todos aquellos archivos que van a ser
 // embebidos por los archivos ".pug", como por ejemplo imagenes, .css, .js, etc.
@@ -46,9 +44,9 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 // Ruta a el formulario para adicionar nuevo estudiante
-app.get('/adicionarEstudiante', (req, res) => {
+app.get("/adicionarEstudiante", (req, res) => {
 	console.log("GET -> adicionarEstudiante")
-	res.render('adicionarEstudiante')
+	res.render("adicionarEstudiante")
 })
 
 /* ==================================================================
@@ -112,30 +110,30 @@ app.get("/reporteVotacionTobo", (req, res) => {
 // ===========================================================================
 // Consultar estudiante para votar
 // 
-app.post('/consultarEstudiantes', (req, res) => {
+app.post("/consultarEstudiantes", (req, res) => {
 	console.log("POST -> consultarEstudiantes: Listado de estudiantes")
 })
 
 // Consultar estudiantes SOLO IE CASCAJAL
-app.get('/consultarEstudiantes', (req, res) => {
+app.get("/consultarEstudiantes", (req, res) => {
 	console.log("GET -> consultarEstudiantes: Listado de estudiantes")
 	Estudiante.
 	find({}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		res.render('consultarEstudiantes', {estudiantes} )
+		res.render("consultarEstudiantes", {estudiantes} )
 	})
 })
 
-app.get('/estudiantesIECascajal', (req, res) => {
+app.get("/estudiantesIECascajal", (req, res) => {
 	console.log("GET -> estudiantesIECascajal: Listado de estudiantes")
 	Estudiante.
 	find({}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		res.render('estudiantesIECascajal', {estudiantes} )
+		res.render("estudiantesIECascajal", {estudiantes} )
 	})
 })
 
@@ -145,12 +143,12 @@ app.get("/sedeIECascajal", (req, res) => {
 	res.render("sedeIECascajal")
 })
 
-app.get('/votarIECascajal', (req, res) => {
+app.get("/votarIECascajal", (req, res) => {
 	console.log("GET -> votar IECascajal" + req.body.gradosIECascajal)
-	res.render('votarIECascajal')
+	res.render("votarIECascajal")
 })
 
-app.post('/votarIECascajal', (req, res) => {
+app.post("/votarIECascajal", (req, res) => {
 
 	nom_sede = "CASCAJAL"
 	num_grado_estudiante = req.body.gradosIECascajal
@@ -169,7 +167,9 @@ app.post('/votarIECascajal', (req, res) => {
 		exec( (error, docs) => {
 			estudiantes = docs
 			registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
-			res.render('votarIECascajal', {estudiantes, registros_a_bloquear} )
+			let sedes = "Listado de estudiantes sede I. E. Cascajal"
+			let volver_a = "/sedeIECascajal"
+			res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 		})			
 
 	} else if( req.body.gradosIECascajal=="SEXTO B" ) {
@@ -179,7 +179,9 @@ app.post('/votarIECascajal', (req, res) => {
 		exec( (error, docs) => {
 			estudiantes = docs
 			registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
-			res.render('votarIECascajal', {estudiantes, registros_a_bloquear} )
+			let sedes = "Listado de estudiantes sede I. E. Cascajal"
+			let volver_a = "/sedeIECascajal"
+			res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 		})	
 	} else if( req.body.gradosIECascajal=="OCTAVO A" ) {
 		Estudiante.
@@ -188,7 +190,9 @@ app.post('/votarIECascajal', (req, res) => {
 		exec( (error, docs) => {
 			estudiantes = docs
 			registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
-			res.render('votarIECascajal', {estudiantes, registros_a_bloquear} )
+			let sedes = "Listado de estudiantes sede I. E. Cascajal"
+			let volver_a = "/sedeIECascajal"
+			res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 		})			
 
 	} else if( req.body.gradosIECascajal=="OCTAVO B" ) {
@@ -198,7 +202,9 @@ app.post('/votarIECascajal', (req, res) => {
 		exec( (error, docs) => {
 			estudiantes = docs
 			registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
-			res.render('votarIECascajal', {estudiantes, registros_a_bloquear} )
+			let sedes = "Listado de estudiantes sede I. E. Cascajal"
+			let volver_a = "/sedeIECascajal"
+			res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 		})	
 	} else {
 		Estudiante.
@@ -207,7 +213,9 @@ app.post('/votarIECascajal', (req, res) => {
 		exec( (error, docs) => {
 			estudiantes = docs
 			registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
-			res.render('votarIECascajal', {estudiantes, registros_a_bloquear} )
+			let sedes = "Listado de estudiantes sede I. E. Cascajal"
+			let volver_a = "/sedeIECascajal"
+			res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 		})		
 	}
 
@@ -219,24 +227,34 @@ app.get("/sedeElTobo", (req, res) => {
 	res.render("sedeElTobo")
 })
 
-app.get('/votarSedeElTobo', (req, res) => {
+app.get("/votarSedeElTobo", (req, res) => {
 	console.log("GET -> votar votarSedeElTobo" + req.body.gradosSedeElTobo)
-	res.render('votarSedeElTobo')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("listarEstudiantesVotacion")
 })
 
-app.post('/votarSedeElTobo', (req, res) => {
+app.post("/votarSedeElTobo", (req, res) => {
 	console.log("POST -> votar votarSedeElTobo" + req.body.gradosSedeElTobo)
 
 	nom_sede = "EL TOBO"
 	num_grado_estudiante = req.body.gradosSedeElTobo
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedeElTobo', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede El Tobo"
+		let volver_a = "/sedeElTobo"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
 })
 // ============================================================================
@@ -245,25 +263,36 @@ app.get("/sedeLaEsperanza", (req, res) => {
 	res.render("sedeLaEsperanza")
 })
 
-app.get('/votarSedeLaEsperanza', (req, res) => {
+app.get("/votarSedeLaEsperanza", (req, res) => {
 	console.log("GET -> votar votarSedeLaEsperanza" + req.body.gradosSedeLaEsperanza)
-	res.render('votarSedeLaEsperanza')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("votarSedeLaEsperanza")
 })
 
-app.post('/votarSedeLaEsperanza', (req, res) => {
+app.post("/votarSedeLaEsperanza", (req, res) => {
 	console.log("POST -> votar votarSedeLaEsperanza" + req.body.gradosSedeLaEsperanza)
 
 	nom_sede = "LA ESPERANZA"
 	num_grado_estudiante = req.body.gradosSedeLaEsperanza
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedeLaEsperanza', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede La Esperanza"
+		let volver_a = "/sedeLaEsperanza"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
+
 })
 
 // ============================================================================
@@ -272,24 +301,34 @@ app.get("/sedeLaPiragua", (req, res) => {
 	res.render("sedeLaPiragua")
 })
 
-app.get('/votarSedeLaPiragua', (req, res) => {
+app.get("/votarSedeLaPiragua", (req, res) => {
 	console.log("GET -> votar votarSedeLaPiragua" + req.body.gradosSedeLaPiragua)
-	res.render('votarSedeLaPiragua')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("votarSedeLaPiragua")
 })
 
-app.post('/votarSedeLaPiragua', (req, res) => {
+app.post("/votarSedeLaPiragua", (req, res) => {
 	console.log("POST -> votar votarSedeLaPiragua" + req.body.gradosSedeLaPiragua)
 
 	nom_sede = "LA PIRAGUA"
 	num_grado_estudiante = req.body.gradosSedeLaPiragua
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedeLaPiragua', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede La Piragua"
+		let volver_a = "/sedeLaPiragua"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
 })
 
@@ -299,24 +338,34 @@ app.get("/sedePaquies", (req, res) => {
 	res.render("sedePaquies")
 })
 
-app.get('/votarSedePaquies', (req, res) => {
+app.get("/votarSedePaquies", (req, res) => {
 	console.log("GET -> votar votarSedePaquies" + req.body.gradosSedePaquies)
-	res.render('votarSedePaquies')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("votarSedePaquies")
 })
 
-app.post('/votarSedePaquies', (req, res) => {
+app.post("/votarSedePaquies", (req, res) => {
 	console.log("POST -> votar votarSedePaquies" + req.body.gradosSedePaquies)
 
 	nom_sede = "PAQUIES"
 	num_grado_estudiante = req.body.gradosSedePaquies
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedePaquies', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede Paquies"
+		let volver_a = "/sedePaquies"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
 })
 
@@ -327,24 +376,34 @@ app.get("/sedeLaFlorida", (req, res) => {
 	res.render("sedeLaFlorida")
 })
 
-app.get('/votarSedeLaFlorida', (req, res) => {
+app.get("/votarSedeLaFlorida", (req, res) => {
 	console.log("GET -> votar votarSedeLaFlorida" + req.body.gradosSedeLaFlorida)
-	res.render('votarSedeLaFlorida')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("votarSedeLaFlorida")
 })
 
-app.post('/votarSedeLaFlorida', (req, res) => {
+app.post("/votarSedeLaFlorida", (req, res) => {
 	console.log("POST -> votar votarSedeLaFlorida" + req.body.gradosSedeLaFlorida)
 
 	nom_sede = "LA FLORIDA"
 	num_grado_estudiante = req.body.gradosSedeLaFlorida
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedeLaFlorida', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede La Florida"
+		let volver_a = "/sedeLaFlorida"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
 })
 
@@ -355,24 +414,34 @@ app.get("/sedeMateoRico", (req, res) => {
 	res.render("sedeMateoRico")
 })
 
-app.get('/votarSedeMateoRico', (req, res) => {
+app.get("/votarSedeMateoRico", (req, res) => {
 	console.log("GET -> votar votarSedeMateoRico" + req.body.gradosSedeMateoRico)
-	res.render('votarSedeMateoRico')
+	num_id_estudiante = req.body.documentoIdentidadEstudiante
+	res.render("votarSedeMateoRico")
 })
 
-app.post('/votarSedeMateoRico', (req, res) => {
+app.post("/votarSedeMateoRico", (req, res) => {
 	console.log("POST -> votar votarSedeMateoRico" + req.body.gradosSedeMateoRico)
 
 	nom_sede = "MATEO RICO"
 	num_grado_estudiante = req.body.gradosSedeMateoRico
+
+	Votante.
+	find( {"vot_sede": nom_sede, "vot_grado":num_grado_estudiante} ).
+	select( {_id:0, votante_doc_identificacion:1} ).
+	exec( (error, docs) => {
+		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
+	})
 
 	Estudiante.
 	find({"est_grado": num_grado_estudiante, "est_nombre_sede": nom_sede}).
 	select({est_tipo_identificacion:1, est_doc:1, est_primer_apellido:1, est_segundo_apellido:1, est_primer_nombre:1, est_segundo_nombre:1, est_grado:1, est_grupo:1, est_matricula_contratada:1, est_fuente_recursos:1}).
 	exec( (error, docs) => {
 		estudiantes = docs
-		registros_a_bloquear = bloquearRegistros(estudiantes, obtener_ids_estudiantes_ya_votaron(nom_sede, num_grado_estudiante))
-		res.render('votarSedeMateoRico', {estudiantes, registros_a_bloquear} )
+		registros_a_bloquear = bloquearRegistros(estudiantes, ids_estudiantes_ya_votaron)
+		let sedes = "Listado de estudiantes sede Mateo Rico"
+		let volver_a = "/sedeMateoRico"
+		res.render("listarEstudiantesVotacion", {sedes, volver_a, estudiantes, registros_a_bloquear} )
 	})
 })
 
@@ -439,16 +508,6 @@ app.post("/finalProcesoVotacion", (req, res) => {
 
 // ============================================================================
 // 		
-// 
-function obtener_ids_estudiantes_ya_votaron( nomSede, numGradoEstudiante) {
-	Votante.
-	find( {"vot_sede": nomSede, "vot_grado":numGradoEstudiante} ).
-	select( {_id:0, votante_doc_identificacion:1} ).
-	exec( (error, docs) => {
-		ids_estudiantes_ya_votaron = obtener_ids_estudiantes_ya_votaron(docs)
-	})
-	return ids_estudiantes_ya_votaron
-}
 
 function obtener_ids_estudiantes_ya_votaron(listaYaVotaron) {
 	// Arreglo de numeros de identificación de estudiantes que ya votarion
